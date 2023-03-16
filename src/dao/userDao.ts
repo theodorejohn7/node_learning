0c5fb927a1e107aec8c65421fe130946c29b5b0c
TheodoreJohn4299 <theodorejohn4299@gmail.com>
2023-03-17 04:38:10 +0530

    implemented get, create from dao file

--- src/dao/userDao.ts
+++ src/dao/userDao.ts
@@ -1,3 +1,35 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true },
+    password: { type: String, required: true },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
+  });
+
+  const UserModel = mongoose.model('User', userSchema);
+
+  class UserDao {
+    
+    async createUser(user: any): Promise<any> {
+      const createdUser = await UserModel.create(user);
+      return createdUser.toObject();
+    }
+
+    async getAllUsers(): Promise<any[]> {
+        const users = await UserModel.find().exec();
+        return users.map((user) => user.toObject());
+      }
+ 
+
+}
+export const userDao = new UserDao();
+
+
+
+
+
 // import  User  from '../models/user';
 // import { MongoClient, Db, Collection } from 'mongodb';
 
--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,15 +1,27 @@
 
 import User from "../models/user";
+import { userDao } from "../dao/userDao";
 
-export default class UserService {
-  async getAllUsers() {
-    const users = await User.find();
-    return users;
+ class UserService {
+  // async getAllUsers() {
+  //   const users = await User.find();
+  //   return users;
+  // }
+
+  // async createUser(user: { name: string; email: string; password: string }) {
+  //   const newUser = await User.create(user);
+  //   return newUser;
+  // }
+
+  async getAllUsers(): Promise<any[]> {
+    return userDao.getAllUsers();
   }
 
-  async createUser(user: { name: string; email: string; password: string }) {
-    const newUser = await User.create(user);
-    return newUser;
+  async createUser(user:  { name: string; email: string; password: string }): Promise<any> {
+    return userDao.createUser(user);
   }
 
 }
+
+
+export const userService = new UserService();
