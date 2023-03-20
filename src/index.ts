69627001992d2116af8f8e1c4e8bf8c58a53fefb
TheodoreJohn4299 <theodorejohn4299@gmail.com>
2023-03-20 11:55:32 +0530

    added cors to project

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
+import cors from "cors"; 
 import dotenv from "dotenv";
 import express from "express";
 import bodyParser from "body-parser";
-import { dbConnect } from "./utils/db"; 
-import router from "./routes";
+import { dbConnect } from "./utils/db";
 import routes from "./routes";
-
-import UserRouter from "./routes/userRoutes"
 
 dotenv.config();
 
@@ -22,24 +20,20 @@ class App {
   private config(): void {
     this.app.use(express.json());
     this.app.use(bodyParser.urlencoded({ extended: true }));
+    this.app.use(cors());
   }
 
   private routes(): void {
-    const userRouter = new UserRouter();
- 
     this.app.use(routes);
-
   }
 
   private connectToDatabase(): void {
-  
     dbConnect.connectDB();
 
     const port = process.env.PORT_NO || 3777;
     this.app.listen(port, () => {
       console.log(`Server running on port ${port}`);
     });
-
   }
 }
 
